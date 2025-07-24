f36244c34254c0cdb8186f37dab8c2c2446f417b
Balaji Bandgar <balajibandgar26@gmail.com>
2025-07-24 12:37:52 +0530

    Taskbar

--- src/Desktop.jsx
+++ src/Desktop.jsx
@@ -1,6 +1,6 @@
 function Desktop(){
     return(
-        <div className="w-screen h-screen bg-gradient-to-br from-red-300 to-black text-white p-4">
+        <div className="w-screen h-screen text-white p-4 bg-[url(https://images.pexels.com/photos/1172675/pexels-photo-1172675.jpeg)] bg-cover bg-center">
             <h1 className="text-3xl font-bold">Welcome to my OS</h1>
         </div>
     )
--- src/Taskbar.jsx
+++ src/Taskbar.jsx
@@ -1,8 +1,28 @@
+import { BatteryMedium, CloudSun, Search, Volume2, Wifi } from "lucide-react";
+
 function Taskbar(){
 
     return(
-        <div className="fixed bottom-0 left-0 w-full h-12 bg-black/70 backdrop-blur-sm flex items-center px-4">
-            <p className="text-white text-sm">🕘 10:23 AM</p>
+        <div className="fixed bottom-0 left-0 w-full h-12 bg-black/70 backdrop-blur-sm flex items-center justify-between px-4 text-white">
+            <div><CloudSun /></div>
+
+            <div className="flex items-center gap-2">
+                <input type="text" className="border-1 rounded-2xl p-1" placeholder=" 🔍Search" />
+                <div>Apps</div>
+            </div>
+
+            <div className="flex items-center gap-2">
+                <div className="flex items-center gap-2 p-2 rounded-md hover:bg-gray-500">
+                    <Wifi size={20} />
+                    <Volume2 size={20}/>
+                    <BatteryMedium size={20} /></div>
+                <div className="text-[11px] gap-2 px-2 rounded-md hover:bg-gray-500">
+                    <div>
+                        <p>12:34 PM</p>
+                        <p>24-07-2025</p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 };
