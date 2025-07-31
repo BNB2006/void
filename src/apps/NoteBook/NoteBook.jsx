78d7ef754bc7a85a7b707e7c6deb7ae31af18f39
Balaji Bandgar <balajibandgar26@gmail.com>
2025-07-31 20:06:26 +0530

    Notebook lines fixed

--- src/apps/NoteBook/NoteBook.jsx
+++ src/apps/NoteBook/NoteBook.jsx
@@ -3,8 +3,8 @@ export function NoteBook() {
     return(
 
         <div class="h-full bg-gray-200 bg-[length:100%_1.5em] [background-image:repeating-linear-gradient(to_bottom,transparent_0_calc(1.5em-1px),black_calc(1.5em-1px)_1.5em)] ">
-            <div class="absolute left-11 top-0 bottom-0 w-[1px] bg-red-500"></div>
-            <div class="absolute left-12 top-0 bottom-0 w-[1px] bg-red-500"></div>
+            <div class="absolute left-11 top-9.5 bottom-0 w-[1px] bg-red-500"></div>
+            <div class="absolute left-12 top-9.5 bottom-0 w-[1px] bg-red-500"></div>
             <textarea class="w-full h-full bg-transparent outline-none resize-none text-gray-800 font-mono leading-[1.5em] pl-13 pt-1" placeholder="🖋️ Write Something......"></textarea>
         </div>
     )
