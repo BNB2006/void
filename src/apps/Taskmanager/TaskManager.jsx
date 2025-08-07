e1746af4be5c25c50c6051eecd8355b5f0e90d3f
Balaji Bandgar <balajibandgar26@gmail.com>
2025-08-07 19:42:22 +0530

    layout

--- src/apps/Taskmanager/TaskManager.jsx
+++ src/apps/Taskmanager/TaskManager.jsx
@@ -1,7 +1,34 @@
 export function Taskmanager(){
     return(
-        <div>
-            Taskmanager: Under development....⚙️
+        <div className="bg-gray-700 h-full text-white">
+            <div className="border-b border-gray-600 p-2">Performance</div>
+            <div className="flex h-160">
+                <div className="bg-red-400 w-60 p-2">
+                    <div className="p-2 text-2xl">CPU</div>
+                    <div className="p-2 text-2xl">Memory</div>
+                    <div className="p-2 text-2xl">Disk</div>
+                    <div className="p-2 text-2xl">Wi-Fi</div>
+                    <div className="p-2 text-2xl">GPU</div>
+                </div>
+
+                <div className="bg-blue-400 flex-1 flex flex-col p-2 gap-2">
+                    <div>CPU</div>
+                    <div className="flex flex-col gap-4">
+                        <div className="flex flex-1 gap-2">
+                            <div className="bg-red-400 w-150 h-50">1</div>
+                            <div className="bg-green-400 w-150 h-50">2</div>
+                        </div>
+                        <div className="flex flex-1 gap-2">
+                            <div className="bg-yellow-400 w-150 h-50">3</div>
+                            <div className="bg-purple-400 w-150 h-50" w-100 h-100>4</div>
+                        </div>
+                    </div>
+
+                    <div className="bg-red-400 w-302 h-30"></div>
+
+                    <div className="bg-red-400 w-302 h-40"></div>
+                </div>
+            </div>
         </div>
     )
 }
