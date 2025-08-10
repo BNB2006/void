4c99d106117f551fe3113ea82ae09a77ebb9e1f8
Balaji Bandgar <balajibandgar26@gmail.com>
2025-08-10 17:33:55 +0530

    Camera logic

--- src/apps/Camera/Camera.jsx
+++ src/apps/Camera/Camera.jsx
@@ -1,8 +1,30 @@
-export function Camera() {
-    return(
-        <div>
-            <p>You photo📷 was click the movement you open this app.</p>
-            <p>You are looking sigma, we will expose you 💀</p>
-        </div>
-    )
-}
+import { CameraIcon } from "lucide-react";
+import { useRef, useState } from "react";
+import Webcam from "react-webcam";
+
+export function Camera(){
+  const webcamRef = useRef(null);
+  const [images, setImages] = useState([]);
+
+  const capture = () => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    setImages((prev) => [...prev, imageSrc]);
+  };
+
+  return (
+        <>
+        <Webcam
+            audio={false}
+            ref={webcamRef}
+            screenshotFormat="image/jpeg"
+            style={{ width: "100%", maxWidth: "500px" }}
+          />
+          <br />
+          <button onClick={capture}><CameraIcon/></button>
+
+          {images.map((image) => (
+            <img src={image} alt="Captured" style={{ width: "100%", maxWidth: "500px" }} />
+          ))}
+        </>
+  );
+};
