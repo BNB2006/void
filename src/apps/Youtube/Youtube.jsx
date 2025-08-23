8b4d1b7400ac4f6bb7b15521887bc9ba6fa361d9
Balaji Bandgar <balajibandgar26@gmail.com>
2025-08-23 16:54:40 +0530

    more videos

--- src/apps/Youtube/Youtube.jsx
+++ src/apps/Youtube/Youtube.jsx
@@ -18,51 +18,267 @@ export function Youtube(){
     const [likes, setLikes] = useState([]);
 
     const [videos, setVideos] = useState([
-        {id:1, title:"Bal Hanuman Official Trailer Digus Isekai", src:"https://www.youtube.com/embed/_NJvncbpcCA?si=gYlq9oPETJE0ZAcL", 
-            thumbnail:"https://img.youtube.com/vi/_NJvncbpcCA/maxresdefault.jpg", views:"389K", postDate:"3 days", channelName:"Diguslsekai", isLiked:false, isDisliked:false,
-            profilePicture:"https://yt3.googleusercontent.com/fy3YHjK3ligSA0Dv7ukFO8I9DRrAh3mJjMBSLpf6_fi5G8jmCWmtveAq2Ge3_xuSFvvpJJoHoxI=s160-c-k-c0x00ffffff-no-rj", subscribers:"24.7M"},
-        {
-            id:2, title:"Scott Lang Training Montage | Ant-Man | Official Clip", src:"https://www.youtube.com/embed/jRWvpJNovZ0?si=Kwt3WU8ZAMeqSihD", 
-            thumbnail:"https://img.youtube.com/vi/jRWvpJNovZ0/maxresdefault.jpg", views:"301K", postDate:"1 day", channelName:"Marvel Entertainment", isLiked:false, isDisliked:false,
-            profilePicture:"https://yt3.googleusercontent.com/k7BhK-hm9_MbJbaKznHPhir6e4pWXbm1ppAHoseLIzRgoAPBMmH1IIhYKlXbGono25RD1OQwHQ=s160-c-k-c0x00ffffff-no-rj", subscribers:"21.7M"
-        },
-        {
-            id:3, title:"Happiness", src:"https://www.youtube.com/embed/e9dZQelULDk?si=5_xYYMfcL7f6rlfF", 
-            thumbnail:"https://img.youtube.com/vi/e9dZQelULDk/maxresdefault.jpg", views:"55M", postDate:"7 year", channelName:"Steve Cutts", isLiked:false, isDisliked:false,
-            profilePicture:"https://yt3.googleusercontent.com/ytc/AIdro_miKmb89rERFj1lwkCnn7IY7bazxfNuAApGexoZFNI-v-0=s160-c-k-c0x00ffffff-no-rj", 
-            subscribers:"1.97M"
-        },
-        {
-            id:4, title:"Dormammu Time Loop | Doctor Strange | Official Clip", src:"https://www.youtube.com/embed/DCrFkaZL254?si=RC7YN9XA2Lvf28_B", 
-            thumbnail:"https://img.youtube.com/vi/DCrFkaZL254/maxresdefault.jpg", views:"3.4M", postDate:"3 months", channelName:"Marvel Entertainment", isLiked:false, isDisliked:false, 
-            profilePicture:"https://yt3.googleusercontent.com/k7BhK-hm9_MbJbaKznHPhir6e4pWXbm1ppAHoseLIzRgoAPBMmH1IIhYKlXbGono25RD1OQwHQ=s160-c-k-c0x00ffffff-no-rj", subscribers:"21.7M"
-        },
-        {
-            id:5, title:"Claude Code is Amazing!", src:"https://www.youtube.com/embed/i3tRkbsE54Q?si=dCklvzbO-rAwIMVH", 
-            thumbnail:"https://img.youtube.com/vi/i3tRkbsE54Q/maxresdefault.jpg", views:"166K", postDate:"3 weeks", channelName:"CodeWithHarry", isLiked:false, isDisliked:false,
-            profilePicture:"https://yt3.googleusercontent.com/ytc/AIdro_kX3sdbuu3KFmRPsmlu0R5Rx_BhpxwupjtvJmkEdNfla7w=s160-c-k-c0x00ffffff-no-rj", subscribers:"8.75M"
-        },
-        {
-            id:6, title:"Loki Bhaiya | Udit Edits", src:"https://www.youtube.com/embed/mM7gfVYwFuI?si=ERAu_axKBqmKpz4R", 
-            thumbnail:"https://img.youtube.com/vi/mM7gfVYwFuI/maxresdefault.jpg", views:"315K", postDate:"1 year", channelName:"Udit Edits", isLiked:false, isDisliked:false,
-            profilePicture:"https://yt3.googleusercontent.com/DNGLrro0sbqheMKEgNE3UF7b8IgBO_HysH4YcVMOYIcymZHBb8keTZ-4aa3x7C5z8-n-zJ92=s160-c-k-c0x00ffffff-no-rj", subscribers:"123K"
-        },
-        {
-            id:7, title:"Lord of the Mysteries | Official Trailer 2 | [ENG SUB]", src:"https://www.youtube.com/embed/02jcxBEIWpg?si=OZFTu5kDPtwBwaRC",  isDisliked:false,
-            thumbnail:"https://img.youtube.com/vi/02jcxBEIWpg/maxresdefault.jpg", views:"807K", postDate:"1 year", channelName:"MemozaFusion", isLiked:false,
-            profilePicture:"https://yt3.googleusercontent.com/MxxxHmYiH2BHmB1cdoDubP4h1gTikFMVTXRzgBxU6LfvBGX5agT2ajECMTZVCrSRCXTsQdBVFg=s160-c-k-c0x00ffffff-no-rj", subscribers:"73.8K"
-        },
-        {
-            id:8, title:"Infinity Ultron VS The Watcher | What If...? | Official Clip", src:"https://www.youtube.com/embed/0tMMzqWIbcw?si=m5bDsqt5n54aofw9", 
-            thumbnail:"https://img.youtube.com/vi/0tMMzqWIbcw/maxresdefault.jpg", views:"259K", postDate:"2 weeks", channelName:"Marvel Entertainment", isLiked:false, isDisliked:false,
-            profilePicture:"https://yt3.googleusercontent.com/k7BhK-hm9_MbJbaKznHPhir6e4pWXbm1ppAHoseLIzRgoAPBMmH1IIhYKlXbGono25RD1OQwHQ=s160-c-k-c0x00ffffff-no-rj", subscribers:"21.7M"
-        },
-        {
-            id:9, title:"Afusic - Pal Pal (Official Music Video) Prod.", src:"https://www.youtube.com/embed/8of5w7RgcTc?si=ySS_Xs5d5GJ7-XuV", 
-            thumbnail:"https://img.youtube.com/vi/8of5w7RgcTc/maxresdefault.jpg", views:"259K", postDate:"6 months", channelName:"AFUSIC", isLiked:false, isDisliked:false,
-            profilePicture:"https://yt3.googleusercontent.com/BHwT3bbu6tRGHKppQcBf5DY7arDKCijJesyKFSZGQegRPmQaMsg5j25NzIQGRPISvO6Cq2rL=s160-c-k-c0x00ffffff-no-rj", subscribers:"756K"
-        }
-    ])
+  {
+    id: 1,
+    title: "Bal Hanuman Official Trailer Digus Isekai",
+    src: "https://www.youtube.com/embed/_NJvncbpcCA?si=gYlq9oPETJE0ZAcL",
+    thumbnail: "https://img.youtube.com/vi/_NJvncbpcCA/maxresdefault.jpg",
+    views: "389K",
+    postDate: "3 days",
+    channelName: "Diguslsekai",
+    isLiked: false,
+    isDisliked: false,
+    profilePicture:
+      "https://yt3.googleusercontent.com/fy3YHjK3ligSA0Dv7ukFO8I9DRrAh3mJjMBSLpf6_fi5G8jmCWmtveAq2Ge3_xuSFvvpJJoHoxI",
+    subscribers: "24.7M",
+  },
+  {
+    id: 2,
+    title: "Scott Lang Training Montage | Ant-Man | Official Clip",
+    src: "https://www.youtube.com/embed/jRWvpJNovZ0?si=Kwt3WU8ZAMeqSihD",
+    thumbnail: "https://img.youtube.com/vi/jRWvpJNovZ0/maxresdefault.jpg",
+    views: "301K",
+    postDate: "1 day",
+    channelName: "Marvel Entertainment",
+    isLiked: false,
+    isDisliked: false,
+    profilePicture:
+      "https://yt3.googleusercontent.com/k7BhK-hm9_MbJbaKznHPhir6e4pWXbm1ppAHoseLIzRgoAPBMmH1IIhYKlXbGono25RD1OQwHQ",
+    subscribers: "21.7M",
+  },
+  {
+    id: 3,
+    title: "Happiness",
+    src: "https://www.youtube.com/embed/e9dZQelULDk?si=5_xYYMfcL7f6rlfF",
+    thumbnail: "https://img.youtube.com/vi/e9dZQelULDk/maxresdefault.jpg",
+    views: "55M",
+    postDate: "7 year",
+    channelName: "Steve Cutts",
+    isLiked: false,
+    isDisliked: false,
+    profilePicture:
+      "https://yt3.googleusercontent.com/ytc/AIdro_miKmb89rERFj1lwkCnn7IY7bazxfNuAApGexoZFNI-v-0",
+    subscribers: "1.97M",
+  },
+  {
+    id: 4,
+    title: "Dormammu Time Loop | Doctor Strange | Official Clip",
+    src: "https://www.youtube.com/embed/DCrFkaZL254?si=RC7YN9XA2Lvf28_B",
+    thumbnail: "https://img.youtube.com/vi/DCrFkaZL254/maxresdefault.jpg",
+    views: "3.4M",
+    postDate: "3 months",
+    channelName: "Marvel Entertainment",
+    isLiked: false,
+    isDisliked: false,
+    profilePicture:
+      "https://yt3.googleusercontent.com/k7BhK-hm9_MbJbaKznHPhir6e4pWXbm1ppAHoseLIzRgoAPBMmH1IIhYKlXbGono25RD1OQwHQ",
+    subscribers: "21.7M",
+  },
+  {
+    id: 5,
+    title: "Claude Code is Amazing!",
+    src: "https://www.youtube.com/embed/i3tRkbsE54Q?si=dCklvzbO-rAwIMVH",
+    thumbnail: "https://img.youtube.com/vi/i3tRkbsE54Q/maxresdefault.jpg",
+    views: "166K",
+    postDate: "3 weeks",
+    channelName: "CodeWithHarry",
+    isLiked: false,
+    isDisliked: false,
+    profilePicture:
+      "https://yt3.googleusercontent.com/ytc/AIdro_kX3sdbuu3KFmRPsmlu0R5Rx_BhpxwupjtvJmkEdNfla7w",
+    subscribers: "8.75M",
+  },
+  {
+    id: 6,
+    title: "Loki Bhaiya | Udit Edits",
+    src: "https://www.youtube.com/embed/mM7gfVYwFuI?si=ERAu_axKBqmKpz4R",
+    thumbnail: "https://img.youtube.com/vi/mM7gfVYwFuI/maxresdefault.jpg",
+    views: "315K",
+    postDate: "1 year",
+    channelName: "Udit Edits",
+    isLiked: false,
+    isDisliked: false,
+    profilePicture:
+      "https://yt3.googleusercontent.com/DNGLrro0sbqheMKEgNE3UF7b8IgBO_HysH4YcVMOYIcymZHBb8keTZ-4aa3x7C5z8-n-zJ92",
+    subscribers: "123K",
+  },
+  {
+    id: 7,
+    title: "Lord of the Mysteries | Official Trailer 2 | [ENG SUB]",
+    src: "https://www.youtube.com/embed/02jcxBEIWpg?si=OZFTu5kDPtwBwaRC",
+    isDisliked: false,
+    thumbnail: "https://img.youtube.com/vi/02jcxBEIWpg/maxresdefault.jpg",
+    views: "807K",
+    postDate: "1 year",
+    channelName: "MemozaFusion",
+    isLiked: false,
+    profilePicture:
+      "https://yt3.googleusercontent.com/MxxxHmYiH2BHmB1cdoDubP4h1gTikFMVTXRzgBxU6LfvBGX5agT2ajECMTZVCrSRCXTsQdBVFg",
+    subscribers: "73.8K",
+  },
+  {
+    id: 8,
+    title: "Infinity Ultron VS The Watcher | What If...? | Official Clip",
+    src: "https://www.youtube.com/embed/0tMMzqWIbcw?si=m5bDsqt5n54aofw9",
+    thumbnail: "https://img.youtube.com/vi/0tMMzqWIbcw/maxresdefault.jpg",
+    views: "259K",
+    postDate: "2 weeks",
+    channelName: "Marvel Entertainment",
+    isLiked: false,
+    isDisliked: false,
+    profilePicture:
+      "https://yt3.googleusercontent.com/k7BhK-hm9_MbJbaKznHPhir6e4pWXbm1ppAHoseLIzRgoAPBMmH1IIhYKlXbGono25RD1OQwHQ",
+    subscribers: "21.7M",
+  },
+  {
+    id: 9,
+    title: "Afusic - Pal Pal (Official Music Video) Prod.",
+    src: "https://www.youtube.com/embed/8of5w7RgcTc?si=ySS_Xs5d5GJ7-XuV",
+    thumbnail: "https://img.youtube.com/vi/8of5w7RgcTc/maxresdefault.jpg",
+    views: "259K",
+    postDate: "6 months",
+    channelName: "AFUSIC",
+    isLiked: false,
+    isDisliked: false,
+    profilePicture:
+      "https://yt3.googleusercontent.com/BHwT3bbu6tRGHKppQcBf5DY7arDKCijJesyKFSZGQegRPmQaMsg5j25NzIQGRPISvO6Cq2rL",
+    subscribers: "756K",
+  },
+  {
+    id: 10,
+    title:
+      "Monica - Telugu Song| COOLIE | Superstar Rajinikanth | Sun Pictures | Lokesh | Anirudh | Pooja Hegde",
+    src: "https://www.youtube.com/embed/x6OaIcF3hbQ",
+    thumbnail: "https://img.youtube.com/vi/KKKBaaxH_Jk/maxresdefault.jpg",
+    views: "4.8M",
+    postDate: "1 month",
+    channelName: "Sun Neo TV",
+    isLiked: false,
+    isDisliked: false,
+    profilePicture:
+      "https://yt3.ggpht.com/35trRALtSCYVdtZcAK-4ixGWDfsVBzLCVc9lBZlkZ7XGPqdYrD-Mf7iSlR_2J_P7gORpZoj7Vg",
+    subscribers: "1.77M",
+  },
+  {
+    id: 11,
+    title: "ONE PIECE: Season 2 | First Look | Netflix",
+    src: "https://www.youtube.com/embed/-UE2i8xXErU",
+    thumbnail: "https://img.youtube.com/vi/-UE2i8xXErU/maxresdefault.jpg",
+    views: "7.4",
+    postDate: "25 days",
+    channelName: "Netflix",
+    isLiked: false,
+    isDisliked: false,
+    profilePicture:
+      "https://yt3.ggpht.com/CvgBA1ypUZNxOjiCX0l1V2FbAm7oSDPZE4YkMvkpT_4iLXQ3IXWVtBgWnznHxgtcUoj50TXqZA",
+    subscribers: "31.5M",
+  },
+  {
+    id: 12,
+    title: "Maand (Lyrics) - Bayaan, Hasan Raheem, Rovalio",
+    src: "https://www.youtube.com/embed/p6ca7gq5H70",
+    thumbnail: "https://img.youtube.com/vi/p6ca7gq5H70/maxresdefault.jpg",
+    views: "15M",
+    postDate: "7 months",
+    channelName: "Euphonies",
+    isLiked: false,
+    isDisliked: false,
+    profilePicture:
+      "https://yt3.ggpht.com/gipjgVyErCewJEpj1tmg2oa0hNS-zSgU32e_sK40MOPea-oBi9DwSnjR-gcrkyx2gESLU73vnw",
+    subscribers: "39.1K",
+  },
+  {
+    id: 13,
+    title:
+      "When Spider-Man Lost His Powers! 😢| Spider-Man 2’s Most Emotional Scene | Hindi Dubbed - Superhero",
+    src: "https://www.youtube.com/embed/HRan3NeCtZs",
+    thumbnail: "https://img.youtube.com/vi/HRan3NeCtZs/maxresdefault.jpg",
+    views: "196K",
+    postDate: "2 weeks",
+    channelName: "Flimy Action",
+    isLiked: false,
+    isDisliked: false,
+    profilePicture:
+      "https://yt3.ggpht.com/bpO1snAa9XXrLfQUpX-4m3yjGLMuU5Q1ChM_fs6vtTpOzxN1enjlK2Z-4j9cuiXDsVEQ2qZ5mw",
+    subscribers: "2.13M",
+  },
+  {
+    id: 14,
+    title:
+      "Installing VS Code & How Websites Work | Sigma Web Development Course - Tutorial #1",
+    src: "https://www.youtube.com/embed/tVzUXW6siu0",
+    thumbnail: "https://img.youtube.com/vi/tVzUXW6siu0/maxresdefault.jpg",
+    views: "5.7M",
+    postDate: "1 year",
+    channelName: "CodeWithHarry",
+    isLiked: false,
+    isDisliked: false,
+    profilePicture:
+      "https://yt3.ggpht.com/ytc/AIdro_kX3sdbuu3KFmRPsmlu0R5Rx_BhpxwupjtvJmkEdNfla7w",
+    subscribers: "8.77M",
+  },
+  {
+    id: 15,
+    title: "The Most Important Film of the Year ⋮ Dhadak 2 Movie Review",
+    src: "https://www.youtube.com/embed/j46gBkSscw0",
+    thumbnail: "https://img.youtube.com/vi/j46gBkSscw0/maxresdefault.jpg",
+    views: "314K",
+    postDate: "2 weeks",
+    channelName: "PJ Explained",
+    isLiked: false,
+    isDisliked: false,
+    profilePicture:
+      "https://yt3.ggpht.com/ytc/AIdro_kZDPy1Lt5dc0Doto1Dqb-uG24R6B1ZkeH7E58r4NMUxkE",
+    subscribers: "2.52M",
+  },
+  {
+    id: 16,
+    title:
+      "[Otherworldly Munchkin: Let's Speedrun the Dungeon with Only 1 HP! | Official Trailer | Crunchyroll",
+    src: "https://www.youtube.com/embed/Clr6kEIs5co",
+    thumbnail: "https://img.youtube.com/vi/Clr6kEIs5co/maxresdefault.jpg",
+    views: "73K",
+    postDate: "4 weeks",
+    channelName: "Crunchyroll",
+    isLiked: false,
+    isDisliked: false,
+    profilePicture:
+      "https://yt3.ggpht.com/BUp_RGMNx21asWdmkCSN0X9QoBAARyCXxG_cnC-Zd4_8xARZDgRDVjbhSa99jmoy4z9bdbNy",
+    subscribers: "7.71M",
+  },
+  {
+    id: 17,
+    title:
+      "Demon Slayer: Kimetsu no Yaiba Infinity Castle | OFFICIAL ENGLISH DUB TRAILER",
+    src: "https://www.youtube.com/embed/U85ASAbHPzU",
+    thumbnail: "https://img.youtube.com/vi/U85ASAbHPzU/maxresdefault.jpg",
+    views: "486K",
+    postDate: "1 month",
+    channelName: "Aniplex USA",
+    isLiked: false,
+    isDisliked: false,
+    profilePicture:
+      "https://yt3.ggpht.com/ytc/AIdro_n4xlOv990XVpqUNNcBE1wY9C6nb0LYE9jjsPM80DlA3qw",
+    subscribers: "995K",
+  },
+  {
+    id: 18,
+    title:
+      "Bye Bye Bye Opening Scene | DEADPOOL & WOLVERINE (2024) Movie CLIP HD",
+    src: "https://www.youtube.com/embed/VHAK-gU9gi0",
+    thumbnail:
+      "https://i.ytimg.com/vi/VHAK-gU9gi0/hqdefault.jpg?sqp=-oaymwEnCNACELwBSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLA9ra0FWlhL6qUtQmQfuTrRhi-R7Q",
+    views: "40M",
+    postDate: "9 month",
+    channelName: "JoBlo Movie Clips",
+    isLiked: false,
+    isDisliked: false,
+    profilePicture:
+      "https://yt3.ggpht.com/_2dYGk34OLMvQPN5co7uQ9OX3_vSOVI4PExNuaNTFRW1lsCcmhQrxJ46C0HwWhxxhdyXsA75IxI",
+    subscribers: "7.71M",
+  },
+]);
+
 
     const playVideo = (videoDetails) => {
         const videoInVideos = videos.find(v => v.id === videoDetails.id);
@@ -316,15 +532,15 @@ export function Youtube(){
                                         <div onClick={() => playVideo(video)} className="flex gap-4 cursor-pointer flex-1">
                                             <img src={video.thumbnail} alt="" className="w-48 h-32 object-cover rounded-md"/>
                                             <div className="flex flex-col justify-start">
-                                                <span className="text-lg font-medium mb-2">{video.title}</span>
-                                                <div className="flex items-center gap-2 mb-2">
+                                                <span className="text-lg font-medium">{video.title}</span>
+                                                <div className="flex items-center gap-2 py-1.5">
                                                     <img src={video.profilePicture} className="w-6 h-6 rounded-full" alt="" />
                                                     <span className="text-sm text-gray-400 flex items-center gap-2">
                                                         {video.channelName} 
                                                         <CircleCheckBig className="w-3 text-blue-500"/> 
                                                     </span>
                                                 </div>
-                                                <div className="flex items-center gap-2 text-sm text-gray-400 mb-2">
+                                                <div className="flex items-center gap-2 text-sm text-gray-400">
                                                     <span>{video.views} views</span>
                                                     <span>|</span>
                                                     <span>{video.postDate} ago</span>
